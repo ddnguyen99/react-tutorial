b5b200f5d2acffd7d3849af033858514912cc76b
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-06 17:51:26 +0700

    Finish Import and Export Component

---
+++ src/Gallery.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Profile from "./Profile.jsx";
+
+function Gallery() {
+    return (
+        <section>
+            <h1> Amazing scientists </h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+}
+
+export default Gallery
