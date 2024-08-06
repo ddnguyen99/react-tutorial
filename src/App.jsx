c08402dfdb5072f52369a0763ec7511f885aaf5a
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-06 18:46:25 +0700

    Learn Change HTML to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Gallery from "./Gallery.jsx";
+import Todolist from "./Todolist.jsx";
 
 function App() {
 
   return (
-      <Gallery/>
-  )
+      <Todolist/>
+      )
 }
 
 export default App
